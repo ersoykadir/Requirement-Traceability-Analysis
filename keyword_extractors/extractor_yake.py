51b1fe27f5ab76a7d120a7328da2e165f8681109
Ecenur Sezer <64018144+codingAku@users.noreply.github.com>
2023-04-01 21:43:24 +0300

    Extractor_yake has been changed to a class.

--- keyword_extractors/extractor_yake.py
+++ keyword_extractors/extractor_yake.py
@@ -1,14 +1,9 @@
 import yake
 
-sourcefile = open("Extraction.txt", "w")
-for line in open("Requirements.txt"):
-    text = line
-    print(text, file=sourcefile)
-    kw_extractor = yake.KeywordExtractor(top=10, stopwords=set(line.strip() for line in open('SmartStopword.txt')))
-    keywords = kw_extractor.extract_keywords(text.lower())
-    keywords = sorted(keywords, key=lambda x: x[1], reverse=True)
-    for kw, v in keywords:
-        print("Keyphrase: ", kw, ": score", v, file=sourcefile)
-    print("\n", file=sourcefile)
+class Extractor:
 
-sourcefile.close()
+    def extract(self, text):
+        kw_extractor = yake.KeywordExtractor(top=10, stopwords=set(line.strip() for line in open('SmartStopword.txt')))
+        keywords = kw_extractor.extract_keywords(text.lower())
+        keywords = sorted(keywords, key=lambda x: x[1], reverse=True)
+        return keywords
