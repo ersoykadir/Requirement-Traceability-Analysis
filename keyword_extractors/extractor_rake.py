1b7ca653d97b4c69d6349e676406c53c71fc5dc4
Ecenur Sezer <64018144+codingAku@users.noreply.github.com>
2023-04-01 21:41:00 +0300

    Extractor_rake has been made a class with extract function

--- keyword_extractors/extractor_rake.py
+++ keyword_extractors/extractor_rake.py
@@ -3,15 +3,15 @@ import nltk
 import re
 from rake_nltk import Rake
 
-pattern = r"[A-Za-z][^\.!?:]*[\.\!?:]"
-sourcefile = open("Extraction_rake.txt", "w")
-for line in open("Requirements.txt"):
-    r = Rake(stopwords=set(line.strip() for line in open('SmartStopword.txt')))
-    result = re.findall(pattern, line)
-    text = result[0]
-    r.extract_keywords_from_text(text.lower())
-    print(text, file=sourcefile)
-    print(r.get_ranked_phrases(), file=sourcefile)
-    print("\n", file=sourcefile)
 
-sourcefile.close()
+class Extractor:
+    def __init__(self):
+        self.pattern = r"[A-Za-z][^\.!?:]*[\.\!?:]"
+
+    def extract(text):
+        r = Rake(stopwords=set(line.strip() for line in open('SmartStopword.txt')))
+        result = re.findall(pattern, text)
+        text = result[0]
+        r.extract_keywords_from_text(text.lower())
+        return r.get_ranked_phrases()
+
