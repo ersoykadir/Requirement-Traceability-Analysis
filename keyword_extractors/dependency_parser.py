8aeeb6544111e1153d835beb6ab9e57e9ece4a5c
Kadir Ersoy <kadir.ersoy@boun.edu.tr>
2023-04-29 16:08:55 +0300

    parser and extractor modified for custom stopwords path

--- keyword_extractors/dependency_parser.py
+++ keyword_extractors/dependency_parser.py
@@ -2,8 +2,8 @@ import spacy
 import re
 nlp = spacy.load("en_core_web_sm")
 
-dep_file = open("dependency_links.txt", "w")
-req_file = open("Requirements_group2.txt", "r")
+dep_file = open("dependency_links_group3.txt", "w")
+req_file = open("Requirements_group3.txt", "r")
 pattern = r"[A-Za-z][^.!?:()]*"
 for line in req_file:
     result = re.findall(pattern, line)
@@ -11,13 +11,16 @@ for line in req_file:
     piano_doc = nlp(piano_text)
     dep_file.write("{}\n".format(line))
     for token in piano_doc:
+        children = [t.text for t in token.children]
         dep_file.write(
-        f"""
-    TOKEN: {token.text}
-    =====
-    {token.tag_ = }
-    {spacy.explain(token.tag_)}
-    {token.head.text = }
-    {token.dep_ = }
-    {spacy.explain(token.dep_)}\n\n"""
+            f"""
+            TOKEN: {token.text}
+            =====
+            {token.tag_ = }
+            {spacy.explain(token.tag_)}
+            {token.head.text = }
+            {token.dep_ = }
+            {spacy.explain(token.dep_)}\n\n
+            {children = }
+            """
         )
--- keyword_extractors/extractor_yake.py
+++ keyword_extractors/extractor_yake.py
@@ -1,4 +1,5 @@
 import yake
+import os
 
 class Extractor:
 
@@ -8,8 +9,8 @@ class Extractor:
         keywords = sorted(keywords, key=lambda x: x[1], reverse=True)
         return keywords
 
-def extract_yake(text):
-        kw_extractor = yake.KeywordExtractor(top=10, stopwords=set(line.strip() for line in open('SmartStopword.txt')))
+def extract_yake(text, smart_stopwords_path):
+        kw_extractor = yake.KeywordExtractor(top=10, stopwords=set(line.strip() for line in open(smart_stopwords_path)))
         keywords = kw_extractor.extract_keywords(text.lower())
         keywords = sorted(keywords, key=lambda x: x[1], reverse=True)
         keywords = [x[0] for x in keywords]
